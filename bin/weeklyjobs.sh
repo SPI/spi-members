2e9e1b963801da3ea428a8827f72a2ba08c87129
Clint Adams <clint@debian.org>
2012-07-10 12:01:38 -0600

    Update repository to what exists in chic:/srv/members.spi-inc.org/wml
    This is not what is running on the site.

--- bin/weeklyjobs.sh
+++ bin/weeklyjobs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 DATE=`date +'Membership Report for Week Ending %d %b %Y'`
-/srv/members.debian.org/bin/weekrpt.pl | mail -s "$DATE" spi-private@spi-inc.org
+/srv/members.spi-inc.org/bin/weekrpt.pl | mail -s "$DATE" members@members.spi-inc.org
 
